fdca741ad73aeee6b8b85a4c91348f9f3cceb9e1
Stefan Ganswint <166592974+StefanG1102@users.noreply.github.com>
2025-05-05 18:46:53 +0200

    init swift app

---
+++ DailyLeveling/DailyLeveling/DailyLevelingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyLevelingApp.swift
+//  DailyLeveling
+//
+//  Created by Stefan Ganswint on 05.05.25.
+//
+
+import SwiftUI
+
+@main
+struct DailyLevelingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DailyLeveling/DailyLevelingTests/DailyLevelingTests.swift
@@ -0,0 +1,16 @@
+//
+//  DailyLevelingTests.swift
+//  DailyLevelingTests
+//
+//  Created by Stefan Ganswint on 05.05.25.
+//
+
+import Testing
+
+struct DailyLevelingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
